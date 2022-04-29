e32b4122d4ee8e32336fbcd27d406bf286d0b102
haidara200403 <88344323+haidara200403@users.noreply.github.com>
2022-04-29 15:54:26 +0100

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,13 @@
+function myFunction(element,color) {
+  element.style.color = color;
+}
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "name.txt", true);
+  xhttp.send();
+}
